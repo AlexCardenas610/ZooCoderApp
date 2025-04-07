431a35ac8fba395561cdf072b1c8f805e7fbfb75
Alessandro Cardenas-Engle <ci6117or@go.minnstate.edu>
2025-04-07 22:56:01 +0000

    Initial commit for Zoo Code app

--- ZooCoder/obj/Debug/net8.0/ZooCoder.AssemblyInfo.cs
+++ ZooCoder/obj/Debug/net8.0/ZooCoder.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ZooCoder")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+807444b189129a1cf3735a4d16ffbfbacdb25040")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+30aecf59bbad3d485bfa5979c08aade4a9f31b7d")]
 [assembly: System.Reflection.AssemblyProductAttribute("ZooCoder")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ZooCoder")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
